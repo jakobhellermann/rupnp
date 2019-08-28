4848b518506eb7057bd88c4a013bb400795d8ed3
Jakob Hellermann <jakob.hellermann@protonmail.com>
2019-08-28 20:43:34 +0200

    make upnp::discover return stream instead of Vec

--- examples/discovery.rs
+++ examples/discovery.rs
@@ -1,15 +1,18 @@
+#![feature(stmt_expr_attributes, proc_macro_hygiene)]
+
+use futures_async_stream::for_await;
 #[allow(unused_imports)]
 use ssdp_client::search::SearchTarget;
 use std::time::Duration;
-use upnp::Device;
 
 #[tokio::main]
 async fn main() -> Result<(), upnp::Error> {
     //let search_target = SearchTarget::RootDevice;
     let search_target = "urn:schemas-upnp-org:device:ZonePlayer:1".parse().unwrap();
 
-    let devices: Vec<Device> = upnp::discover(search_target, Duration::from_secs(1)).await?;
-    for device in &devices {
+    #[for_await]
+    for device in upnp::discover(search_target, Duration::from_secs(1)).await? {
+        let device = device?;
         let spec = device.description();
         println!(
             "{} - {} @ {}",
--- src/discovery.rs
+++ src/discovery.rs
@@ -1,22 +1,19 @@
 use crate::{Device, Error};
-use futures_async_stream::for_await;
+use futures::prelude::*;
 use ssdp_client::search::SearchTarget;
 use std::time::Duration;
 
 pub async fn discover(
     search_target: SearchTarget<'_>,
     timeout: Duration,
-) -> Result<Vec<Device>, Error> {
-    let mut devices = Vec::new();
-
-    #[for_await]
-    for ip in ssdp_client::search(search_target, timeout, 3).await? {
-        let uri: hyper::Uri = ip?
-            .location()
-            .parse()
-            .map_err(|e| Error::InvalidResponse(Box::new(e)))?;
-
-        devices.push(Device::from_url(uri).await?);
-    }
-    Ok(devices)
+) -> Result<impl Stream<Item = Result<Device, Error>>, Error> {
+    Ok(ssdp_client::search(search_target, timeout, 3)
+        .await?
+        .map(|search_response| -> Result<hyper::Uri, Error> {
+            search_response?
+                .location()
+                .parse()
+                .map_err(|e| Error::InvalidResponse(Box::new(e)))
+        })
+        .and_then(Device::from_url))
 }
